7b9f94d6827ce9799c0a54497ac54ac5135dae3a
ndongogueye <bayeg2003@gmail.com>
2023-07-12 12:26:10 +0200

    push

---
+++ assets/file.js
@@ -0,0 +1,24 @@
+document.getElementById("generaGrigliaBtn").addEventListener("click", function() {
+    let grigliaDiv = document.getElementById("griglia");
+    grigliaDiv.innerHTML = "";
+
+
+    let righe = 10;
+    let colonne = 10;
+
+    for (let i = 1; i <= righe; i++) {
+      for (let j = 1; j <= colonne; j++) {
+        var cella = document.createElement("div");
+        cella.className = "cella";
+        cella.innerText = (i - 1) * colonne + j;
+
+        cella.addEventListener("click", function() {
+          this.style.backgroundColor = "blue";
+          console.log("Cella cliccata: " + this.innerText);
+        });
+
+        grigliaDiv.appendChild(cella);
+      }
+      grigliaDiv.appendChild(document.createElement("br"));
+    }
+  });
